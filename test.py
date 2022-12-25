e4985fa862d502fd741c4780ce2d5bbae3feb74c
Aleksei Korolev <photoalee@mail.ru>
2022-12-25 16:58:02 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+from app.calc import Calculator
+
+class TestCalculator:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_sucess(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_division_sucess(self):
+        assert self.calc.division(self, 2, 2) == 1
+
+    def test_subtraction_sucess(self):
+        assert self.calc.subtraction(self, 2, 2) == 0
+
+    def test_adding_sucess(self):
+        assert self.calc.adding(self, 2, 2) == 4
